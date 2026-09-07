854ed72911d9e1b769eb0293962721e00ede3c9b
Lewis Lakerink <llakerink@swin.edu.au>
2026-09-08 09:34:13 +1000

    chore(gardener): Extract duplicated fetching-cluster trace string into shared const

--- src/src/http/job.rs
+++ src/src/http/job.rs
@@ -17,8 +17,8 @@ use crate::app::AppState;
 use crate::db::entities::{job, job_history};
 use crate::http::auth::{AuthResult, get_applications};
 use crate::http::utils::{
-    INVALID_CLUSTER_MSG, app_no_cluster_access_msg, db_error, job_id_to_u32, parse_csv_u64,
-    parse_job_steps,
+    FETCHING_CLUSTER_MSG, INVALID_CLUSTER_MSG, app_no_cluster_access_msg, db_error, job_id_to_u32,
+    parse_csv_u64, parse_job_steps,
 };
 use crate::protocol::constants::{
     CANCEL_JOB, DELETE_JOB, JOB_COMPLETION_SOURCE, SUBMIT_JOB, SYSTEM_SOURCE,
@@ -137,7 +137,7 @@ pub async fn create_job(
         ));
     }
 
-    tracing::trace!("HTTP: Fetching cluster '{}'", body.cluster);
+    tracing::trace!(FETCHING_CLUSTER_MSG, body.cluster);
     let cluster = state
         .cluster_manager
         .get_cluster_by_name(&body.cluster)
